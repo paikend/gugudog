997ad838e3773af6b88ae5f5c474f2a84660ef78
SeeunChoi1 <candy970106@gmail.com>
2019-07-26 22:01:39 +0900

    service_all.html

--- gugudog_hackathon/gugudog/views.py
+++ gugudog_hackathon/gugudog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def service_all(request):
+    return render(request, 'service_all.html')
