2d8b62113c1c21e1cbaa767d34369e70c9cc7242
gaetokk <tae918@naver.com>
2019-08-06 22:31:12 +0900

    폰트 변경/서비스뉴 페이지 추가

--- gugudog_hackathon/gugudog_hackathon/urls.py
+++ gugudog_hackathon/gugudog_hackathon/urls.py
@@ -39,4 +39,6 @@ urlpatterns = [
     path('test/', views.test, name='test'),
     path('test2/', views.test2, name='test2'),
     path('test3/', views.test3, name='test3'),
+    path('service_new/', views.service_new, name='service_new'),
+
 ]
