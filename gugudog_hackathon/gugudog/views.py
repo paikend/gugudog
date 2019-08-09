5e473b0efe232cec706835f63b52300db49cf1f7
gaetokk <tae918@naver.com>
2019-08-09 17:17:46 +0900

    수정 / 삭제 버튼 url 추가해주세용

--- gugudog_hackathon/gugudog/views.py
+++ gugudog_hackathon/gugudog/views.py
@@ -191,9 +191,12 @@ def service_detail(request, service_pk):
         'service': service,
     }
     if request.user in service.gudog_users.all():
+        context['yes'] = True
         context['isGuDoged'] = "구독하고 있는 서비스에요!"
+        context['myDelete'] = "삭제하기"
     elif request.user in service.zzim_users.all():
         context['isZzimed'] = "찜한 구독 서비스에요!"
+        context['yes'] = False
     
     return render(request, 'service_detail.html', context)
 
